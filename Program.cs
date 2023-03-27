66a9a3cb27cac3d4244b4a449b4f66cdf51fe5b3
unknown <anderson.lima18pb@gmail.com>
2023-03-26 22:50:15 -0300

    aprendendo-c#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ESTUDANDOPOO
+{
+    class Program
+    {
+        static void Main(string[] args )
+        {
+
+            Pessoa obj = new Pessoa();
+            obj.nome = "anderson";
+            obj.mensagem();
+        }
+
+    }
+}
---
+++ pessoa.cs
@@ -0,0 +1,15 @@
+using System;
+class Pessoa
+{
+    
+    public string nome;
+    public int idade;
+
+
+    // metodo 
+
+    public void mensagem()
+    {
+        Console.WriteLine("voce tem "+idade+" anos ");
+    }
+}
