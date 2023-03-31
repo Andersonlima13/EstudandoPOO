1818650968090188ff3a70420d588c3f28615ce2
unknown <anderson.lima18pb@gmail.com>
2023-03-30 22:54:53 -0300

    método

---
+++ exercicio/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ESTUDANDOPOO
+{
+    class Program
+    {
+        static void Main(string[] args )
+        {
+
+            Aluno obj = new Aluno();
+            obj.nome = "anderson";
+            obj.nota1 = 8;
+            obj.nota2 = 10;
+            obj.msg();
+        }
+
+    }
+}
