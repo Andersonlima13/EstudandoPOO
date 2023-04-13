c11adfdb8218676d521155a79cbf633d9a4903ea
unknown <anderson.lima18pb@gmail.com>
2023-04-12 23:07:55 -0300

    linearSearch

---
+++ DataStructure/LinearSearch/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+public class Program
+{
+    static void Main(String [] args)
+    {
+        int[] array = {9,1,8,55,11,55,22};
+
+        int index = LinearSearch(array, 55);
+        if (index != -1)
+        {
+            Console.WriteLine($"Elemento encontrado na posição {index}" ); 
+        }else
+        {
+             Console.WriteLine("Elemento nao encontrado");
+        }
+
+    static int LinearSearch(int[] array , int value)
+    {
+        for (int i = 0 ; i < array.Length; i++)
+        {
+            if(array[i] == value)
+            {
+                return i;
+            }
+ 
+        } 
+    return -1 ;
+    }
+    }
+}
