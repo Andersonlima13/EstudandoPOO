4751aac5a9e9410a9904cdff9e1d6c53f694ab39
unknown <anderson.lima18pb@gmail.com>
2023-04-13 23:54:47 -0300

    interpolationSearch

---
+++ DataStructure/InterpolationSearch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        
+        int[] array = {1,2,3,4,5,6,7,8,9};
+
+        int index = interpolationSearch(int[] array, int value)
+        {
+            int high = array.Length -1;
+            int low = 0 ;
+
+            while(ValueTuple >= array[low] && value <= array[high] && low <= high)
+            {
+                int probe = low + (high - low ) * (ValueTuple - array[low])/
+                 (array[high] - array[low]); 
+
+                 Console.WriteLine($"provavel {probe}");
+
+                 if (array[probe] <value)
+                 {
+                    low = probe + 1;
+
+                 }
+                 else 
+                 {
+                    high = probe - 1;
+                 }
+            }
+        }
+        return -1;
+
+    
+    }
+
+}
