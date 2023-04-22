bee568989b6e2bb9929c2192e45a34c41c2c6532
unknown <anderson.lima18pb@gmail.com>
2023-04-21 23:51:41 -0300

    query

---
+++ study_c#/query/Program.cs
@@ -0,0 +1,11 @@
+﻿class Query
+{
+    static void Principal()
+    {
+        int[] numbers = {1,2,5,4,9,45,55,4};
+
+        var query = from number in numbers
+                    where number < 10
+                    select number; 
+    }
+}
