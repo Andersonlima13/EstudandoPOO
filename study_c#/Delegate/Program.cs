09292903b71a2c5d0cd6232cdf5bd459a41e853f
unknown <anderson.lima18pb@gmail.com>
2023-04-21 23:29:31 -0300

    delegate

---
+++ study_c#/Delegate/Program.cs
@@ -0,0 +1,18 @@
+﻿class program
+{
+    static void main()
+    {
+        var calculate = new Calculate(Sum);
+        var result = calculate(100,200);
+        Console.WriteLine(result);
+    }
+
+    static int Sum(int a, int b)
+    {
+        return a+b; 
+    }
+}
+
+//de modo gera delegate são tipos 
+
+delegate int Calculate(int x, int y);
