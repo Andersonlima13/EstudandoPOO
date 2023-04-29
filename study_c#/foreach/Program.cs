57f05dae2d98a5c1e5b55bc35dcb7ca4947ad57d
unknown <anderson.lima18pb@gmail.com>
2023-04-28 23:09:47 -0300

    estudos c#

---
+++ study_c#/foreach/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+// imprimindo cada um dos eleemntos 
+class For
+{
+    public static void Main()
+    {
+       string frase = "frase para teste do forearch";
+
+       foreach (char i in frase)
+       {
+        Console.WriteLine(i);
+       }
+    }
+}
