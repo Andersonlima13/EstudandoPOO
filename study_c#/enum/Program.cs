7b30d7470963c0b41076f846ea6e1dc165185b84
unknown <anderson.lima18pb@gmail.com>
2023-04-29 16:30:18 -0300

    enum

---
+++ study_c#/enum/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+    
+// permite criar um tipo de classe o qual teremos valores apenas de leitura , logo um grupo de constantes
+    public class Enumerador
+    {
+        public enum MesesDoAno
+        {
+            janeiro,fevereiro, março, abril, maio , junho , julho , agosto, setembro, outubro , novembro
+        }
+
+     public void ExibirMes(MesesDoAno Mes)
+     {
+     Console.WriteLine($" O mês selecionado foi {Mes}");
+     }
+
+
+    }
+   
---
+++ study_c#/enum/main.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ESTUDANDOPOO
+{
+    class Program
+    {
+        static void Main(string[] args )
+        {
+        Enumerador msg = new Enumerador();
+        msg.ExibirMes(Enumerador.MesesDoAno.setembro);
+        
+        }
+
+    }
+}
